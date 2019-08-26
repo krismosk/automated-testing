ec7baba7a7a29bf3e57d541dc884104bda057f72
Kristina <kristina.tanya@gmail.com>
2019-08-26 11:59:12 -0700

    Created first test for deck instance, hasn't passed yet.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,27 @@
-
-# deck.rb
-
 require_relative 'card'
 
 class Deck
-
+  attr_reader :values, :suits, :cards
   def initialize
-
+    @values = [*(2..10), "Ace", "Jack", "King", "Queen"]
+    @suits = [:hearts, :spades, :clubs, :diamonds]
+    @cards = []
+    
+    
   end
-
+  
   def draw
-    # returns a card
+    # removes a Card from the Deck and
+    # returns the removed Card 
   end
-
+  
   def shuffle
     # shuffles the deck
+    # don't test for randomness, just make sure method can be called
   end
+  
+  def count
+    # returns the number of cards in the Deck
+    
+  end 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -3,7 +3,38 @@ require_relative 'test_helper'
 describe Deck do
   # Write tests for the Deck class.
   # What should you test?
-    # 1.  Expected Behavior
-    # 2.  Edge cases
-
+  # 1.  Expected Behavior
+  # 2.  Edge cases
+  
+  describe "You can create a Deck instance" do
+    # Expect that Deck class can be instantiated with 52 cards 
+    it "Can be created" do 
+      # Arrange
+      test_deck = Deck.new
+      
+      # Act / Assert
+      expect(test_deck.cards.length).must_equal 52
+    end
+    
+    it "Raises an ArgumentError for invalid parameters" do 
+      
+      
+      
+    end
+    
+    
+    
+    
+    
+    
+    
+  end
+  # Expect that .draw can be used on Deck
+  # Expect that .shuffle can be used on Deck
+  # Expect that .count can be used on Deck
+  # Expect that reader methods works for Deck instance variables
+  
+  # Edge: Total cards in a deck can't exceed 52
+  # Edge: Each suit has one of each corresponding value and won't exceed allotted quantities
+  # # i.e. only one instance - 2 of hearts; only one instance of Queen of hearts
 end
