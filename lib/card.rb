1da4565ee100fae016a29b7058c7679874e88f72
Kristina <kristina.tanya@gmail.com>
2019-08-26 08:35:29 -0700

    Completed wave 1. First 2 card tests pass.

--- lib/card.rb
+++ lib/card.rb
@@ -3,16 +3,30 @@
 
 class Card
   attr_reader :value, :suit
-
+  
   def initialize(value, suit)
     @value = value
+    @suits = [:hearts, :spades, :clubs, :diamonds]
+    @values = (1..13).to_a
+    
+    if @suits.include?(suit) == false || value < 1 || value > 13
+      raise ArgumentError.new("Not a valid suit.")
+    end
+    
     @suit = suit
     
-
+    
   end
-
+  
+  
   def to_s
     return "#{value} of #{suit.to_s}"
   end
-
 end
+
+# result = Card.new(12, :clubs)
+# p result
+
+
+
+# suits = [:hearts, :spades, :clubs, :diamonds]
--- test/card_test.rb
+++ test/card_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 
 describe Card do
   describe "You can create a Card instance" do
-
+    
     it "Can be created" do
       [:hearts, :spades, :clubs, :diamonds].each do |suit|
         (1..13).each do |value|
@@ -11,7 +11,7 @@ describe Card do
         end
       end
     end
-
+    
     it "Raises an ArgumentError for invalid parameters" do
       # To raise an ArgumentError in card.rb
       # raise ArgumentError.new
@@ -20,33 +20,33 @@ describe Card do
       expect {Card.new(14, :clovers)}.must_raise ArgumentError
     end
   end
-
-  describe "Testing to_s" do
-
-    it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
-    end
-
-    it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
-      # The current implementation of to_s does not address this feature
-      # Write the test, see it fail, then modify to_s to make it pass!
-      # (Consider writing a helper method!)
-    end
-  end
-
-  describe "Reader methods" do
-
-    it "Can retrieve the value of the card using a `.value`." do
-      # ensure that `.value works as expected`
-    end
-
-    it "Can retrieve the value of the card using a `.suit`." do
-      # ensure that `.suit works as expected returning the symbol of the suit`
-
-    end
-  end
-
+  
+  # describe "Testing to_s" do
+  
+  #   it "to_s returns a readable String value logically for values 2-10" do
+  #     # Test to ensure that to_s works for cards values 2-10
+  #     # for example:  "2 of diamonds"
+  #   end
+  
+  #   it "to_s returns a readable String value for Ace, Jack, Queen, King" do
+  #     # Test to ensure that to_s works for cards values 1, and 11-13
+  #     # For example: "Queen of hearts"
+  #     # The current implementation of to_s does not address this feature
+  #     # Write the test, see it fail, then modify to_s to make it pass!
+  #     # (Consider writing a helper method!)
+  #   end
+  # end
+  
+  # describe "Reader methods" do
+  
+  #   it "Can retrieve the value of the card using a `.value`." do
+  #     # ensure that `.value works as expected`
+  #   end
+  
+  #   it "Can retrieve the value of the card using a `.suit`." do
+  #     # ensure that `.suit works as expected returning the symbol of the suit`
+  
+  #   end
+  # end
+  
 end
