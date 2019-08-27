c9f868d60c1de67640637eeb905130debbf92c88
Kristina <kristina.tanya@gmail.com>
2019-08-27 07:25:40 -0700

    Added .count method for Deck & corresponding test.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,27 +1,41 @@
 require_relative 'card'
 
 class Deck
-  attr_reader :values, :suits, :cards
+  attr_reader :cards
+  
   def initialize
-    @values = [*(2..10), "Ace", "Jack", "King", "Queen"]
-    @suits = [:hearts, :spades, :clubs, :diamonds]
     @cards = []
     
+    # create cards array
+    VALUES.each do |value|
+      SUITS.each do |suit|
+        @cards.push(Card.new(value, suit)) 
+      end
+    end
     
-  end
-  
-  def draw
-    # removes a Card from the Deck and
-    # returns the removed Card 
-  end
-  
-  def shuffle
-    # shuffles the deck
-    # don't test for randomness, just make sure method can be called
   end
   
   def count
-    # returns the number of cards in the Deck
-    
+    return cards.length 
   end 
+  
+  
+  
+  
+  
+  
+  # def draw
+  #   # removes a Card from the Deck and
+  #   # returns the removed Card 
+  # end
+  
+  # def shuffle
+  #   # shuffles the deck
+  #   # don't test for randomness, just make sure method can be called
+  # end
+  
+  
 end
+
+my_deck = Deck.new
+p my_deck.count
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,7 +8,7 @@ describe Deck do
   
   describe "You can create a Deck instance" do
     # Expect that Deck class can be instantiated with 52 cards 
-    it "Can be created" do 
+    it "Can be created with 52 cards in deck" do 
       # Arrange
       test_deck = Deck.new
       
@@ -16,22 +16,26 @@ describe Deck do
       expect(test_deck.cards.length).must_equal 52
     end
     
-    it "Raises an ArgumentError for invalid parameters" do 
-      
-      
-      
-    end
-    
-    
-    
-    
-    
-    
     
   end
+  
+  describe "You can count the number of card in a Deck" do
+    it ".count returns a count of cards" do
+      # Expect that .count can be used on Deck
+      # Arrange 
+      test_deck_size = Deck.new
+      
+      # Act / Assert
+      expect(test_deck_size.cards.length).must_equal 52
+    end
+  end 
+  
+  
+  
+  
   # Expect that .draw can be used on Deck
   # Expect that .shuffle can be used on Deck
-  # Expect that .count can be used on Deck
+  
   # Expect that reader methods works for Deck instance variables
   
   # Edge: Total cards in a deck can't exceed 52
